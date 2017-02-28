a0ea5245f249375f8113a01070b6dec2d7fd3b54
naivatko <oktavian.galang.p@gmail.com>
2017-02-28 19:23:21 +0700

    working on dashboard

--- dashboard.php
+++ dashboard.php
@@ -27,19 +27,35 @@ if($_SESSION['svc_status'] == 0) {
 	  <link rel="stylesheet" type="text/css" href="css/dashboard.css">
 	</head>
 <body>
-<header class="row">
-	<div class="container" id="main-header-div">
-		<div id="welcome" class="row col-sm-4">
-			<h5><b>
-				Selamat Datang! <?php echo $username ?>
-			</b></h5>
-		</div>
-		<div class="" id="logout">
-			<a href="modules/logout.php" class="btn btn-default" id="logout-link">
-		  	<span class="glyphicon glyphicon-log-out"></span> Log out
-			</a>
+<header class="container-fluid">
+	<div class="container" id="navbar">
+		<div class="row" id="navbar-main">
+			<div class="col-sm-6 col-xs-6" id="navbar-left">
+				<div id="welcome"><b>Selamat Datang! <?php echo $username; ?></b></div>
+			</div>
+			<div class="col-sm-6 col-xs-6" id="navbar-right">
+				<div id="logout">
+					<a href="modules/logout.php">
+						<button type="button" class="btn btn-default btn-sm">
+	          	<span class="glyphicon glyphicon-log-out" style="color: #2a3242;"></span><b style="color: #2a3242;"> Log out</b>
+	        	</button>
+					</a>
+				</div>
+			</div>
 		</div>
 	</div>
 </header>
+<div class="container row" id="main">
+	<div class="box-group col-sm-6" id="box-group-kiri">
+		<div class="boxes" id="box1">
+			<div>Nickname</div>
+			<div id="nickname"><?php echo $nickname; ?></div>
+		</div>
+		<div class="boxes" id="box2">BOX 2</div>
+	</div>
+	<div class="box-group col-sm-6" id="box-group-kanan">
+		<div class="boxes" id="box3">BOX 3</div>
+		<div class="boxes" id="box4">BOX 4</div>
+	</div>
 </body>
 </html>
