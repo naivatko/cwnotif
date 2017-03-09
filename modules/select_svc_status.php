f53888bdf0ea2ddae1d2a0d8566d29bb21a4741c
naivatko <oktavian.galang.p@gmail.com>
2017-03-10 05:38:35 +0700

    fixed javascript on dashboard

--- modules/select_svc_status.php
+++ modules/select_svc_status.php
@@ -7,4 +7,6 @@
   $svc_status             = mysqli_fetch_assoc($svc_status_query);
   $svc_status             = $svc_status['svc_status'];
   $_SESSION['svc_status'] = $svc_status;
- ?>
+
+  echo $_SESSION['svc_status'];
+?>
