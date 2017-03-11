63b579cd746214956fc0d163c77a0e30e92dc278
naivatko <oktavian.galang.p@gmail.com>
2017-03-11 08:40:49 +0700

    working on update svc status

--- javascript/dashboard.handler.js
+++ javascript/dashboard.handler.js
@@ -4,21 +4,30 @@ $(document).ready(function(){
     if($('#svc-check').prop('checked')){
       $('#svc-status-off').hide();
       $('#svc-status-on').show();
-      svc_status = 1;
+      svc_status = '1';
     } else {
       $('#svc-status-on').hide();
       $('#svc-status-off').show();
-      svc_status = 0;
+      svc_status = '0';
     }
   };
 
-  if(svc_status == 1){
+  function getSvc(){
+    $.post("modules/select_svc_status.php", function(data){
+      svc_status = data;
+    });
+  };
+
+  if(svc_status == '1'){
     $('#svc-check').prop('checked', true);
   }
 
   checkThis();
-
   $('#svc-check').change(function(){
+    console.log("svc status sebelum update = "+svc_status);
     checkThis();
+    $.post("modules/update_svc_status.php",{svc_status:svc_status},function(data){
+      console.log("jawaban server "+data);
+    });
   });
 });
--- modules/update_svc_status.php
+++ modules/update_svc_status.php
@@ -1,6 +1,17 @@
 <?php
-  include("session.php");
   include("connect.php");
-  $select_svc_status  = mysqli_query($db_connect, "UPDATE tbl_user SET svc_status = '$session_svc_status' WHERE user_name = $session_username");
-  $session_svc_status = $select_svc_status['svc_status'];
+  include("session.php");
+
+  $svc_status         = $_POST['svc_status'];
+  $update_svc_status  = mysqli_query($db_connect, "UPDATE tbl_user
+                                                   SET svc_status  = '$svc_status'
+                                                   WHERE user_name = $username");
+
+  if($update_svc_status){
+    $_SESSION['svc_status'] = $svc_status;
+    echo $svc_status;
+  } else {
+    $svc_status = $_SESSION['svc_status'];
+    echo $svc_status;
+  }
 ?>
